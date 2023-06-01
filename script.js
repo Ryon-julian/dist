3e1a70906f0dc63ed45f5603b2d6f98c47ac5eb1
ryon-julian <ryon.anggara1997@gmail.com>
2023-06-02 01:07:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let colorClass = [
+  'satu',
+  'dua',
+  'tiga',
+  'empat',
+  'lima',
+  'enam',
+  'tujuh',
+  'delapan',
+  'sembilan',
+  'sepuluh',
+  'sebelas',
+  'duabelas',
+  'tigabelas',
+  'empatbelas',
+  'limabelas',
+  'enambelas',
+  'tujuhbelas',
+  'delapanbelas',
+  'sembilanbelas',
+  'duapuluh',
+] 
+
+// Function to toggle color classes
+// function toggleColor() {
+//   const svg = document.getElementById('my-svg');
+// colorClass.forEach( (colorClass ,index) => {
+//   svg.classList.remove(...colorClass);
+//   svg.classList.toggle(colorClass);
+// });
+  // var element = svg.classList.togle
+  // svg.classList.remove("satu");
+  // svg.classList.toggle('dua');
+  // svg.classList.remove("dua");
+  // svg.classList.toggle('tiga');
+  // svg.classList.remove("tiga");
+  // svg.classList.toggle('empat');
+  // svg.classList.remove("empat");
+  // svg.classList.toggle('lima');
+  // svg.classList.remove("lima");
+  // svg.classList.toggle('enam');
+  // svg.classList.remove("enam");
+  // svg.classList.toggle('tujuh');
+  // svg.classList.remove("tujuh");
+  // svg.classList.toggle('delapan');
+  // svg.classList.toggle('sembilan');
+  // svg.classList.toggle('sepuluh');
+  // svg.classList.toggle('sebelas');
+  // svg.classList.toggle('duabelas');
+  // svg.classList.toggle('tigabelas');
+  // svg.classList.toggle('empatbelas');
+  // svg.classList.toggle('limabelas');
+  // svg.classList.toggle('enambelas');
+  // svg.classList.toggle('tujuhbelas');
+  // svg.classList.toggle('delapanbelas');
+  // svg.classList.toggle('sembilanbelas');
+  // svg.classList.toggle('duapuluh');
+// }
+// toggleColor();
+// Set interval to toggle color every 20 seconds
+// setInterval(toggleColor,4000);
+
+
+function tes(){
+
+}
+
+
+setInterval(tes, 4000)
